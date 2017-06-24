ad7c41556f4b969717a9f5bd7c2a5a58df7a8b73
spudgi <calebgmiller@gmail.com>
2017-06-23 20:40:25 -0700

    Add files via upload
    added Circumference to program

--- Week_1/Area.java
+++ Week_1/Area.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 public class Area {
 	public static void main(String[] args) {
 		int r;
-		double pi = 3.14, area;//Declare a constant
-		Scanner s = new Scanner(System.in);//Create a Scanner object
-		System.out.print("Enter radius of circle:");//Prompt the user to enter a radius
-		r = s.nextInt();
-		area = pi * r * r;//Compute area
-		System.out.println("Area of circle:"+area);//Display result
+        double pi = 3.14, area;//Declare a constant
+        Scanner s = new Scanner(System.in);//Create a Scanner object
+        System.out.print("Enter radius of circle:");//Prompt the user to enter a radius
+        r = s.nextInt();
+        area = pi * r * r;//Compute area
+        double circumference = 2 * pi * r;
+        System.out.println("Area of circle:"+area);//Display result
+        System.out.println("Circumference of circle:"+circumference);
+        
 	}
 }
