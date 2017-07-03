ec52ce3c84921357b7beae0be29b7856b86882ef
spudgi <calebgmiller@gmail.com>
2017-07-02 22:28:33 -0700

    Add files via upload

--- Week_3/PigLatin.java
+++ Week_3/PigLatin.java
@@ -13,12 +13,12 @@ public class PigLatin {
 	            Sentence = input.nextLine().toUpperCase();
 	            words = Sentence.split(" ");
 	            for (String word : words) {
-	                if (word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u"))
-	                    System.out.print(word.toUpperCase() + "WAY ");
+	                if (word.startsWith("A") || word.startsWith("E") || word.startsWith("I") || word.startsWith("O") || word.startsWith("U"))
+	                    System.out.print(word + "WAY");
 	                else if (word.startsWith("sh") || word.startsWith("ch") || word.startsWith("th"))
-	                    System.out.print(word.substring(2)+word.substring(0,2).toUpperCase()+"AY ");
+	                    System.out.print(word.substring(2)+word.substring(0,2) + "AY");
 	                else
-	                    System.out.print(word.substring(1)+word.substring(0,1).toUpperCase()+"AY ");
+	                    System.out.print(word.substring(1)+word.substring(0,1) + "AY");
 	            }
 			System.out.println();
 	        } while(!Sentence.equals("quit"));
