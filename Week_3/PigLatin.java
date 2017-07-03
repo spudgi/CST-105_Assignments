170143b8cfc20fff83a53211f6ad48cacaa5c093
spudgi <calebgmiller@gmail.com>
2017-07-02 22:37:31 -0700

    Add files via upload

--- Week_3/PigLatin.java
+++ Week_3/PigLatin.java
@@ -14,11 +14,11 @@ public class PigLatin {
 	            words = Sentence.split(" ");
 	            for (String word : words) {
 	                if (word.startsWith("A") || word.startsWith("E") || word.startsWith("I") || word.startsWith("O") || word.startsWith("U"))
-	                    System.out.print(word + "WAY");
+	                    System.out.print(word + "WAY ");
 	                else if (word.startsWith("sh") || word.startsWith("ch") || word.startsWith("th"))
-	                    System.out.print(word.substring(2)+word.substring(0,2) + "AY");
+	                    System.out.print(word.substring(2)+word.substring(0,2) + "AY ");
 	                else
-	                    System.out.print(word.substring(1)+word.substring(0,1) + "AY");
+	                    System.out.print(word.substring(1)+word.substring(0,1) + "AY ");
 	            }
 			System.out.println();
 	        } while(!Sentence.equals("quit"));
