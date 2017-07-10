23d3ca44bb47c4597e2c9d5feb1718d1fcba8a04
spudgi <calebgmiller@gmail.com>
2017-07-09 18:01:03 -0700

    Add files via upload

--- Week_4/TwoDArray.java
+++ Week_4/TwoDArray.java
@@ -1,37 +1,37 @@
 //Caleb Miller//
 	//CST-105//
 		//20170709//
-			//I love this class! I can't get enough of it, although this week was pretty hard for me in terms of this project//
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+			//I love this class but this kicked by butt!//
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class TwoDArray {
+	private static char arrays[][];
 
-	private static Scanner inputFile;
-
-	public static void main(String[] args) throws IOException {
-	int x = inputFile.nextInt(); 
-	int y = inputFile.nextInt(); 
-		while (inputFile.hasNext()) { 
-		char [][] array = new char [x][y];
-		   //char c = words.charAt(i);  
-		    for (int row = 0; row < x; row++) {
-		        for (int col = 0; col < y; col++) {
-		            String words = inputFile.nextLine();
-		            for (int i = 0; i < words.length(); i++)
-		            array[x][y] = words.charAt(i);
-		            for (int i = 0; i < words.length(); i++);
-		        }
-		    }
+	public static void main(String[] args) {
+		
+		String fileName = JOptionPane.showInputDialog(null, "Enter file name and path");
+		Scanner fileInput =null;
+		try {
+			fileInput = new Scanner(new File(fileName));
+		} catch (FileNotFoundException e) {
+			System.err.print("Cannot open the file!");
+			System.exit(0);
 		}
-	}
-	public static String[][] read() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("\test.txt"));
-		  int columnCount = 20;
-		  int rowCount = 45;
-		  inputFile = new Scanner(br);
-		return null;
+		while(fileInput.hasNextLine()) {
+			String line = fileInput.nextLine();
+			arrays = new char[20][45];
+			for (char row = 0; row < 20; row++) {
+				for (char col = 0; col < 45; col++) {
+				System.out.print(arrays[row][col]);
+				}
+			System.out.println(line);
+			}
+			
+		}
+		
 	}
 }
+
