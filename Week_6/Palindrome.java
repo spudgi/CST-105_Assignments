f296b78bdd3a535ea94d39b0ffae39eede06c110
Caleb Miller <calebgmiller@gmail.com>
2017-07-23 20:36:58 -0700

    Add files via upload

--- Week_6/Palindrome.java
+++ Week_6/Palindrome.java
@@ -1,47 +1,29 @@
+//Caleb Miller
+	//2170720	
+		//I should have taken my math classes after these classes :}
 public class Palindrome {
-	public static boolean checkIfPrime (int num) {
-        boolean isPrime = true;  
-        for (int i = 2; i <= 100000; i++) {
-            if ( num % i == 0 ){
-                isPrime = false;
-            }
-        }
-        return isPrime;
-    }	
-	public static void isPalindrome(Integer a) {
-	    String num=Integer.toString(a);
-	    int length=num.length()-1;
-	    //System.out.println(num);
-	    if(num.length()<=1) {
-	        System.out.println("" + num);
-
-	    }
-	    else if(num.length()==2) {
-	        if(num.charAt(0)==num.charAt(1))
-	            System.out.println(num);
-	    }
-	    else if(num.length()>2) {
-	        //now deal with the numbers whose length is greater than 2
-	        for(int i=0;i<=length;i++) {
-	            if(num.charAt(i)==num.charAt(length-i))
-	                System.out.println(num);
-	        }
-	    }
-	}
-	private static int reverse(int i) {
-		int result = 0;
-		while (i > 0) {
-			result = result * 10 + (i % 10);
-			i = i / 10;
+	public static boolean isPrime(int n) {
+		for (int i = 2; i <= n/2; i++) {
+			if (n % i == 0) {
+				return false;
+			}
 		}
-		return result;
+		return (n != 1) ? true : false;
+	}
+	public static boolean isPalindrome(int n){
+		int primeNum = n;
+		int palindNum = 0;
+		while(primeNum > 0) {
+			palindNum = (palindNum * 10) + primeNum % 10;
+			primeNum = primeNum / 10;
+		}
+		return n == palindNum;
 	}
 	public static void main(String[] args) {
-		for ( int i = 0; i < 100000; i++ ) {
-            if ( i == reverse(i) ) {
-                System.out.println( i );
-            }
-        }
+		for(int i = 1; i <= 100000; i++) {
+			if(isPrime(i) && isPalindrome(i)){
+				System.out.println(i);
+			}
+		}
 	}
-	
 }
